87361f74ad48d51ce988f14163a273441b5a1c7f
Joey Yang <joeyyang.ai@gmail.com>
2020-06-03 15:44:43 +0000

    functional sms messaging functionality

--- scripts/intruder_alerter_node.py
+++ scripts/intruder_alerter_node.py
@@ -6,10 +6,15 @@ from twilio.rest import Client
 import smtplib
 from email.message import EmailMessage
 
-account_sid = "" 
-auth_token = ""
+account_sid = "AC06aefadef3a9e078b57b83ac9aa607cd" 
+auth_token = "28aef006228bd6e96939941b78e35fa1"
 
-#client = Client(account_sid, auth_token)
+client = Client(account_sid, auth_token)
+client.messages.create(
+  to = "+16479663926",
+  from_ = "+16475030604",
+  body = "ALERT: INTRUDER DETECTED BY GUARDIAN SURVEILLANCE"
+)
 
 sender_email = "guardiansurveillance.ai@gmail.com"
 sender_password = "t2vqh97u"
@@ -27,14 +32,14 @@ msg.set_content("ALERT: INTRUDER DETECTED BY GUARDIAN SURVEILLANCE")
 server = smtplib.SMTP(smtp_server, starttls_port)
 server.starttls()
 server.login(sender_email, sender_password)
-#server.sendmail(sender_email, receiver_email, msg.as_string())
+server.sendmail(sender_email, receiver_email, msg.as_string())
 server.quit()
 
 def objectCb(object_msg):
-	if (object_msg.classification "Human" == and object_msg.confidence == 0.99):
-		pass 
-	else:
-		rospy.loginfo("ALERT: INTRUDER DETECTED BY GUARDIAN SURVEILLANCE")	
+  if (object_msg.classification "Human" == and object_msg.confidence == 0.99):
+    pass 
+  else:
+    rospy.loginfo("ALERT: INTRUDER DETECTED BY GUARDIAN SURVEILLANCE")	
 
 def intruderAlerter():
     rospy.init_node("intruder_alerter_node", anonymous = True)
@@ -43,4 +48,3 @@ def intruderAlerter():
 
 if __name__ == '__main__':
     intruderAlerter()
-
