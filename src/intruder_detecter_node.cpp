d62a54204e9f8fd95a556b282f4dd8d900baf3f6
Joey Yang <joeyyang.ai@gmail.com>
2020-07-08 22:03:39 -0400

    commenting code

--- src/intruder_detecter_node.cpp
+++ src/intruder_detecter_node.cpp
@@ -46,26 +46,33 @@ public:
 
   void imageCb(const sensor_msgs::CompressedImageConstPtr& msg)
   {
+    // Convert ROS image msg to OpenCV image.
     cv::Mat cv_image = cv::imdecode(cv::Mat(msg->data), 1); 
 
     cv::Mat frame_gray;
     cv::cvtColor(cv_image, frame_gray, cv::COLOR_BGR2GRAY);
     cv::equalizeHist(frame_gray, frame_gray);
 
+    // Detect face(s).
     face_cascade.detectMultiScale(frame_gray, faces_);
-    
+   
+    // Reset face count. 
     if (faces_.empty())
     {
       face_count_ = 0;
     }
+    // Check if intruder(s) detected.
     if (!faces_.empty() && faces_.size() != face_count_)
     {
       face_count_ = faces_.size();
       ROS_INFO("Warning! Intruder(s) Detected!");
+      // Save image.
       if (image_saver_client_.call(image_saver_srv_))
       {
+        // Wait for image to be saved.
         ros::Duration(1.0).sleep();
         ROS_INFO("Snapshot of intruder saved.");
+        // Send email alert with image attached.
 	if (email_alerter_client_.call(email_alerter_srv_))
         {
 	  ROS_INFO("Email alert sent.");
@@ -79,6 +86,7 @@ public:
       {
         ROS_ERROR("Failed to call image_saver service.");
       }
+      // Send text alert.
       if (text_alerter_client_.call(text_alerter_srv_))
       {
         ROS_INFO("Text alert sent.");
@@ -89,6 +97,7 @@ public:
       }
     }
     
+    // Draw circle around each face detected.
     for (size_t i = 0; i < faces_.size(); i++)
     {
       cv::Point center(faces_[i].x + faces_[i].width/2, faces_[i].y + faces_[i].height/2);
@@ -102,7 +111,6 @@ public:
 
 int main(int argc, char* argv[])
 {
-
   if (!face_cascade.load(face_cascade_name))
   {
     std::cout << "Error: Unable to locate Haar Cascade xml files!" << std::endl;
@@ -113,5 +121,6 @@ int main(int argc, char* argv[])
   ros::NodeHandle nh;
   ImageProcessor ip(nh);
   ros::spin();
+ 
   return 0;
 }
